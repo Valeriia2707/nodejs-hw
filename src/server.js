67a43e609a69f3ee05d465d9bb6c12ae7de35196
Valeri <valeriakoval27072007@gmail.com>
2026-03-09 21:58:26 +0100

    3

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import notesRoutes from '../src/routes/notesRoutes.js';
+import router from '../src/routes/notesRoutes.js';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
@@ -14,9 +15,11 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 
-app.use(notesRoutes);
+app.use(router);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
